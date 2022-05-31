1c65fbf1fb4dfc8c21072b3e30bc894c5786fd1a
gopalapubhavana <101540110+gopalapubhavana@users.noreply.github.com>
2022-05-31 14:46:26 +0000

    ....

--- length_of_string.c
+++ length_of_string.c
@@ -1,30 +1,35 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+ 
+// A function to check if a string str is palindrome
+void isPalindrome(char str[])
+{
+    // Start from leftmost and rightmost corners of str
+    int l = 0;
+    int h = strlen(str) - 1;
+ 
+    // Keep comparing characters while they are same
+    while (h > l)
+    {
+        if (str[l++] != str[h--])
+        {
+            printf("%s is not a palindrome\n", str);
+            return;
+        }
+    }
+    printf("%s is a palindrome\n", str);
+}
+ 
+// Driver program to test above function
 int main()
 {
-	char str [20];
-	int i,length;
-    int s=0;
-    printf("enter a string");
-    scanf("%s",sprintf);
-    length = strlen(sprintf);
-	for(i=0;i<length;i++)
-	{
-		if(string1[i])1 = string1[length-i-1];)
-		{
-			s=1;
-			break;
-		}
-	}
-	if(s)
-	{
-		printf("%s is not a palindrome",string1);
-	}
-	{
-		printf("%s is a palindrome",string1);
-	}
-	return 0;
+    isPalindrome("abba");
+    isPalindrome("abbccbba");
+    isPalindrome("geeks");
+    return 0;
 }
-
-
-
+/*
+abba is a palindrome
+abbccbba is a palindrome
+geeks is not a palindrome
+*/
