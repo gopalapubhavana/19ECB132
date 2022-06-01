e7ac14511818189e1238d5039957c37187390122
gopalapubhavana <101540110+gopalapubhavana@users.noreply.github.com>
2022-06-01 04:12:40 +0000

    .....

--- lettercount.c
+++ lettercount.c
@@ -11,3 +11,9 @@ int main(void) {
   printf("\nThe number of letters in the given sentence are : %d\n", number_of_letters);
   return 0;
 }
+/*
+output:
+Enter a sentence: bhavana
+
+The number of letters in the given sentence are : 7
+*/
